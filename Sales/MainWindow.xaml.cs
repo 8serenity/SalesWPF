796467c5c51849f6ef44ff4bdb4baa5e7f7b4eaa
Niyaz <33822162+serenity8@users.noreply.github.com>
2018-04-07 22:20:20 +0600

    Done HW

---
+++ Sales/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Collections.ObjectModel;
+
+namespace Sales
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DbConnection newConnection = Connection;
+
+            newConnection.Open();
+
+            DataTable dt = newConnection.GetSchema("Tables");
+
+            List<string> tableNames = new List<string>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row[3].ToString().Equals("BASE TABLE", StringComparison.OrdinalIgnoreCase))
+                {
+                    string tableName = row[2].ToString();
+                    tableNames.Add(tableName);
+                }
+            }
+
+            comboBox.ItemsSource = tableNames;
+            newConnection.Close();
+        }
+
+        private void ChangedTable(object sender, SelectionChangedEventArgs e)
+        {
+            DbCommand command = Connection.CreateCommand();
+
+            command.CommandText = "select * from" + " " + comboBox.SelectedValue.ToString();
+
+            DbDataAdapter adapter = Factory.CreateDataAdapter();
+
+            adapter.SelectCommand = command;
+
+            DataTable tableToDisplay = new DataTable();
+
+            adapter.Fill(tableToDisplay);
+
+            listData.ItemsSource = tableToDisplay.DefaultView;
+        }
+
+        public static DbConnection Connection
+        {
+            get
+            {
+                DbProviderFactory providerFactory = DbProviderFactories.GetFactory(ConfigurationManager
+                                            .ConnectionStrings["SalesConnection"].ProviderName);
+                DbConnection connection = providerFactory.CreateConnection();
+
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["SalesConnection"].ConnectionString;
+                return connection;
+            }
+        }
+
+        public DbProviderFactory Factory
+        {
+            get
+            {
+                DbProviderFactory providerFactory = DbProviderFactories.GetFactory(ConfigurationManager
+                                            .ConnectionStrings["SalesConnection"].ProviderName);
+                return providerFactory;
+            }
+        }
+    }
+}
---
+++ Sales/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales
+{
+    class Sale
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int SalesmanId { get; set; }
+        public decimal TotalSum { get; set; }
+        public DateTime DateSale { get; set; }
+    }
+}
